e9618dabb2354f092547dd9f7256eb6423639ed6
chhaya742 <chhayabagwan34@gmail.com>
2022-04-21 19:45:39 +0530

    first commit

---
+++ Tkiner/project.py
@@ -0,0 +1,31 @@
+# from tkinter import *
+# root=Tk()
+# root.configure(background="red")
+
+# def send():
+#     send="teena-> "+e.get()
+#     txt.insert(END,"\n"+send)
+#     if (e.get()=="hii"):
+#         txt.insert(END,"\n"+"annu->hello")
+#     elif (e.get()=="how are you"):
+#         txt.insert(END,"\n"+"i am fine")
+#     elif (e.get()=="what are you doing"):
+#         txt.insert(END,"\n"+"annu->tkiner GUI")
+#     elif(e.get()=="ok"):
+#         txt.insert(END,"\n"+"and you") 
+#     else:
+#         txt.insert(END,"\n"+"sorry i dint get your")       
+
+    
+#     # pass3
+#     e.delete(0,END)
+# txt=Text(root)
+# txt.grid(row=0,column=0)
+# e=Entry(root,width=5)
+# send=Button(root,text="SEND",command=send).grid(row=1,column=1)
+# e.grid(row=1,column=0)
+# root.title("Chatbot")
+# root.mainloop() 
+
+
+
